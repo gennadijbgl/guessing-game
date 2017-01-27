14656a80eac1a2b8fca03ce4146744defa63e84b
Hienadz <gennadijbgl@gmail.com>
2017-01-27 21:08:34 +0300

    guessing-game/feature: implemented game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this._max = null;
+        this._min = null;
+        this._solution = null;
+    }
 
     setRange(min, max) {
-
+        this._max = max;
+        this._min = min;
     }
 
     guess() {
+        this._solution = Math.round((this._min+this._max)/2);
 
+        return this._solution;
     }
 
     lower() {
-
+        this._max = this._solution;
     }
 
     greater() {
-
+        this._min = this._solution;
     }
 }
 
